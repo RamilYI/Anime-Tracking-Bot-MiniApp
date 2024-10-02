ad170665defd8f97adfca15af779e5fca379eff7
YusupovRI <YusupovRI@bnipi.rosneft.ru>
2024-10-03 00:54:24 +0500

    вернул как было

--- src/components/ListComponent.tsx
+++ src/components/ListComponent.tsx
@@ -38,7 +38,7 @@ export default function ListComponent()
             }
         };
         getSeasonData();
-    }, []);
+    }, [titleParams, seasonsData]);
 
     const [searchItem, setSearchItem] = useState('');
 
