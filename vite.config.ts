d173c3e03c9cbe464c777f8c1f7e76519d7b8f5f
YusupovRI <YusupovRI@bnipi.rosneft.ru>
2024-10-03 00:27:20 +0500

    test deploy

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import basicSsl from '@vitejs/plugin-basic-ssl';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths(), basicSsl(), ],
+  base: "/AnimeTrackingDemo_frontend/",
   server: {
     proxy:{
       '/api/bot/getSeason':{
