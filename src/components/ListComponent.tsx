0a85010ad03a60bb402b4a6f8783e39612c5d6ef
YusupovRI <YusupovRI@bnipi.rosneft.ru>
2024-10-09 23:36:32 +0500

    update path for fetch season

--- src/components/ListComponent.tsx
+++ src/components/ListComponent.tsx
@@ -15,7 +15,7 @@ export default function ListComponent()
     useEffect(() => {
         const getSeasonData = async () => {
             try{
-                const response = await fetch('https://89.111.174.61:443/api/bot/getSeason');
+                const response = await fetch('https://animetracking.duckdns.org/api/bot/getSeason');
                 const data = await response.json() as TitleInformationDto[];
                 const filteredData = data.filter(function (value: TitleInformationDto, index: number, array: TitleInformationDto[]) {
                     const findDtoIndex = array.findIndex(x => value.id == x.id);
