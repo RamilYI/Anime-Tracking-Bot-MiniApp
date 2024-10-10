546efee007b723a156937f629d3038d6934b413f
YusupovRI <YusupovRI@bnipi.rosneft.ru>
2024-10-11 02:47:31 +0500

    update path for fetch season

--- src/components/CardComponent.tsx
+++ src/components/CardComponent.tsx
@@ -1,8 +1,16 @@
 import {TitleInformationDto} from "../Infrastructure/TitleInformationDto.ts";
 import {Cell, Checkbox} from "@telegram-apps/telegram-ui";
+import React, {useState} from "react";
 
 export default function CardComponent({cardDto}:{cardDto: TitleInformationDto})
 {
+    const [titleValues, setTitleValues] = useState(cardDto);
+
+    function handleCheckbox(event: React.ChangeEvent<HTMLInputElement>){
+        setTitleValues({...titleValues, isEnabled:event.target.checked});
+        cardDto.isEnabled = event.target.checked;
+    }
+
     return <Cell
         Component="label"
         before={<img style={{
@@ -12,9 +20,7 @@ export default function CardComponent({cardDto}:{cardDto: TitleInformationDto})
             width: 41.2
         }}
                      src={cardDto.coverImage.large}/>}
-        after={<Checkbox checked={cardDto.isEnabled}  onChange={() => {
-            cardDto.isEnabled = !cardDto.isEnabled;
-        }} />}
+        after={<Checkbox defaultChecked={cardDto.isEnabled} onChange={handleCheckbox}/>}
         subtitle={<p style={{fontSize: "12px", margin: 0}}>?/?</p>}
         multiline
         style={{margin:-10}}>
