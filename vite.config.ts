274eab1340233a5100632f697bf2c947b0d5bee7
YusupovRI <YusupovRI@bnipi.rosneft.ru>
2024-10-31 01:37:16 +0500

    правки имен

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   server: {
     proxy:{
       '/api/bot/getSeason':{
-        target: 'https://animetracking.duckdns.org/',
+        target: 'http://animetracking.duckdns.org/',
         changeOrigin: true,
         secure: false,
       },
